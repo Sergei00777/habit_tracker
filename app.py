10bd9e69ff6955d3c128bd38e3abfaf8b20110eb
“Sergei007” <oni007@mail.ru>
2025-10-09 13:30:57 +0800

    Второй коммит

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)  # Отключаем reloader временно
+    app.run(host='0.0.0.0', port=5000, debug=False)
